c05d818770e78ba9927b94f1063333dd683ca56c
Spencer Stolworthy <sdstolworthy@gmail.com>
2018-06-12 23:30:24 -0600

    switched to typescript

--- index.ts
+++ index.ts
@@ -1,11 +1,12 @@
+require('dotenv').config()
 var credentials = {
-  username: 'sdstolworthy@gmail.com',
-  password: 'ceenrps93S',
+  username: process.env.RH_UNAME,
+  password: process.env.RH_PASS,
 }
 
 var robinhoodSdk = require('robinhood')
 const Robinhood = robinhoodSdk(credentials, () => {
-  Robinhood.quote_data('CEI', function(err, resp, body) {
+  Robinhood.quote_data('CEI', function(err: any, resp: any, body: any) {
     // console.log(body)
     if (body && body.results && body.results[0] && body.results[0].bid_price) {
       var options = {
@@ -18,11 +19,10 @@ const Robinhood = robinhoodSdk(credentials, () => {
         }
       }
       if (err) {
-        console.log(err)
+        //
       }
       // console.log(body, resp)
-      Robinhood.place_buy_order(options, function(error, response, body) {
-        console.log('in buy')
+      Robinhood.place_buy_order(options, function(error: any, response:any, body: any) {
         if(error) {
           console.log(error)
         } else {
